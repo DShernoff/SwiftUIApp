6fd9360955620f3f59aa803fa976ae0e54123a9c
David Shernoff <david.shernoff@rutgers.edu>
2021-04-20 16:07:55 -0400

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by David Shernoff on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
